47dc366dddf3169f1bc779c6c4c7e0f4df2e9a7b
Lê Văn Cường <vancuongle65@gmail.com>
2024-07-09 21:34:44 +0700

    first commit

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println( readFile("test.txt"));
+
+    }
+    //Creat a funtion to Read and a funtion to write to txt file
+    public static void writeToFile(String fileName, String content) {
+        try (FileWriter writer = new FileWriter(fileName, true)) {
+            writer.write(content);
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+    public static String readFile(String fileName) {
+        StringBuilder contentBuilder = new StringBuilder();
+        BufferedReader reader = null;
+        try  {
+            reader = new BufferedReader(new FileReader(fileName, StandardCharsets.UTF_8));
+            String line;
+            while ((line = reader.readLine())!= null) {
+                contentBuilder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading from file: " + e.getMessage());
+        }
+        
+        return contentBuilder.toString();
+    }
+
+}
